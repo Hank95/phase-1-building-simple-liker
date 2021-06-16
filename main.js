dc1dd753386a3aa3b19b7d5b19dbecb6fdb3df1a
github username <github email address>
2021-06-16 11:38:09 -0400

    commit tree 1c0cd98c61b2c1bb15a25a39f9d67e4d21af135c

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+const heartButtons = document.querySelectorAll("header");
+console.log(typeof heartButtons);
+heartButtons.forEach((heart) => {
+  heart.addEventListener("click", (e) => {
+    e.target.innerText = FULL_HEART;
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
